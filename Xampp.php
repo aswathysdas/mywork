5b6f90be9be2ab9322448846d92001c25c878074
ASWATHY S DAS <aswathysdas43@gmail.com>
2022-02-21 18:57:20 +0530

    Xampp.php

--- Xampp.php
+++ Xampp.php
@@ -10,7 +10,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$pp=mysqli_query($conn,"INSERT INTO tblname VALUES ()
+$pp="INSERT INTO.$VALUES ()
     $trp = mysqli_query($conn, "SELECT * FROM statetbl");
     $rows = array();
     while($r = mysqli_fetch_assoc($trp)) {
