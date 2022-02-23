d6276cd43182e8a55c8cc369dda552890aab2400
ASWATHY S DAS <aswathysdas43@gmail.com>
2022-02-23 22:54:41 +0530

    Create actin.php

---
+++ actin.php
@@ -0,0 +1,10 @@
+<?php
+class Blog extends CI_Controller {
+
+       public function __construct()
+       {
+            parent::__construct();
+            // Your own constructor code
+       }
+}
+?>
